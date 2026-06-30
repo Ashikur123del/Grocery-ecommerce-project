9019ff52dd750f7c05c8dc5342340066fdd4f20d
ashik <leadskillit@gmail.com>
2026-06-30 10:21:33 +0600

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ import Category from './Pages/DashboardLayout/Category/Category';
 // import Payments from './Pages/DashboardLayout/Payments/Payments';
 import Inventory from './Pages/DashboardLayout/Inventory/Inventory';
 import DashBoradOverview from './Pages/DashboardLayout/Overview/DashBoradOverview';
-import TermsofService from './Pages/TermsofService/TermsofService';
+// import TermsofService from './Pages/TermsofService/TermsofService';
 // import Analytics from './Pages/DashboardLayout/Analytics/Analytics';
  
 
@@ -75,7 +75,7 @@ export const router = createBrowserRouter([
       { path: "wishlist", element: <Wishlist /> },
       { path: "order-tracking", element: <OrderTracking/> },
       { path: 'privacy-policy', element: <PrivacyPolicy />} ,
-      {path: "terms-of-service", element: <TermsofService />},
+      // {path: "terms-of-service", element: <TermsofService />},
       {path: "return-policy", element: <ReturnPolicy />},
       {path: "faq",  element: <Faqs />},
       {path: "flash-deals",element: <FlashDealsPage />},
